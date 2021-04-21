c46a978e68bfc58ed4f34f7f0514d48459b7b383
shabbir aly <shabbiralikhan326@gmail.com>
2021-04-22 02:40:06 +0500

    chnages

--- src/components/Searching.vue
+++ src/components/Searching.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="search__wrapper container">
     <div class="search__content">
-      <img src="../assets/img/dot.png" alt="" srcset="" />
+      <div class="dots">
+        <img class="one" src="../assets/img/25.png" alt="" srcset="" />
+        <img class="two" src="../assets/img/25.png" alt="" srcset="" />
+      </div>
       <h2>
         Your next customer is searching. <br />
         Make sure they find you.
       </h2>
       <button>Get started</button>
     </div>
+    <img class="three" src="../assets/img/25.png" alt="" srcset="" />
   </div>
 </template>
 <style scoped>
 .search__wrapper {
   height: auto;
   background-color: #3366ff;
+}
+.dots {
+  display: flex;
+  justify-content: space-between;
+}
+.dots .one {
+  height: 30px;
+  width: 124px;
+  margin-left: 150px;
+  margin-top: -25px;
+}
+.dots .two {
+  height: 30px;
+  width: 124px;
+  margin-top: -25px;
+  margin-right: 150px;
+}
+.three {
+  margin-left: 430px;
 }
 .search__wrapper .search__content {
   text-align: center;
